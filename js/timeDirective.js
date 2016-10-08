624a92ff8765d09d9243cd5b6af4bcc99a7d5971
Kathryn Nutzman <kathryn.nutzman@gmail.com>
2016-10-08 10:10:46 -0600

    finished project

---
+++ js/timeDirective.js
@@ -0,0 +1,18 @@
+angular.module('timeApp').directive('showTime', function() {
+return {
+	restrict: 'E',
+	template: '<div>The current time is {{time}}</div>', //use templateUrl: '/something.html' for larger html
+	link: function(scope, element, attrs) {
+		var currentTime = new Date();
+		scope.time = currentTime.toString();
+
+
+	}
+
+
+}
+
+
+
+
+})
